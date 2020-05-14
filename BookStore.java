fe8f2a739a767ea219db6e97326edf0ae273adc7
David Juarez <djuar05@gmail.com>
2020-05-14 09:09:22 -0400

    main file and bookstore class

---
+++ BookStore.java
@@ -0,0 +1,27 @@
+public class BookStore {
+        String bookStoreName;
+        String address;
+        String phoneNumber;
+        String openingTime;
+        String closingTime;
+        int squareFeet;
+        boolean usedBooks;
+
+        public boolean haveUsedBooks(){
+            return true;
+        }
+
+        public void bookStoreOpenToday(){
+            System.out.println("Yes the store is open today");
+        }
+
+        public void openedOnSunday(){
+            System.out.println("No not open on Sunday");
+        }
+
+    public BookStore() {
+
+    }
+
+
+}
